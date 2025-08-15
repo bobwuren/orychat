888e2598306def48ca510f6eb10e5740c7c5822a
Darrylwin <logossoudarryl20@gmail.com>
2025-08-15 13:50:38 +0000

    running on 0.0.0.0

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.use(globalErrorHandler);
 
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => {
+const server = app.listen(PORT, "0.0.0.0", () => {
   console.log(`✅ Serveur running on port ${PORT}`);
   console.log(`📖 Documentation: http://localhost:${PORT}/api-docs`);
 });
